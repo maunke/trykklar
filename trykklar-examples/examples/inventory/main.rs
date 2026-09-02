ba99980daecf7cd7a2f0f4902c3569a42bc32488
Markus Unkel <markus@markusunkel.com>
2026-09-02 21:43:55 +0200

    examples: add images to inventory example
    Signed-off-by: Markus Unkel <markus@markusunkel.com>

--- trykklar-examples/examples/inventory/main.rs
+++ trykklar-examples/examples/inventory/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
-use trykklar::inventory::page::ColorSpacesInventory;
+use trykklar::inventory::page::{ColorSpacesInventory, ImagesInventory};
+use trykklar::pdf::Mm;
 use trykklar::{PageWalker, Pdf};
 
 #[derive(Parser)]
@@ -20,13 +21,19 @@ fn main() -> Result<()> {
 
     for (idx, page) in pages.into_iter().enumerate() {
         let page = page?;
-        let mut walker = PageWalker::new();
         let mut colorspaces_inventory = ColorSpacesInventory::new();
+        let mut images_inventory = ImagesInventory::new();
+
+        let mut walker = PageWalker::new();
         walker.add_processor(&mut colorspaces_inventory);
+        walker.add_processor(&mut images_inventory);
         walker.run(&page)?;
+
         println!(
-            "\n=================\nPage {} [Errors {}]\n=================\n",
+            "\n=====================\nColorSpaces on Page {}\nTotal: {}, Errors: \
+             {}\n======================\n",
             idx,
+            colorspaces_inventory.color_spaces().len(),
             colorspaces_inventory.inderterminate()
         );
         for cs in colorspaces_inventory.color_spaces() {
@@ -35,6 +42,36 @@ fn main() -> Result<()> {
         println!("\nThe following separation names were used:\n");
         for separation_name in colorspaces_inventory.separation_names() {
             println!("- {:?}", separation_name);
+        }
+
+        println!(
+            "\n=====================\nImages on Page {}\nTotal: {}, Errors: \
+             {}\n=====================",
+            idx,
+            images_inventory.painted_images().len(),
+            images_inventory.inderterminate()
+        );
+        for img in images_inventory.painted_images() {
+            let dpi = img.dpi();
+            println!("\n- dpi (x, y): ({:.0}, {:.0})", dpi.x(), dpi.y());
+            if let Some(bbox) = img.bbox::<Mm>() {
+                println!(
+                    "  bbox (mm): origin ({:.2}, {:.2}), width x height ({:.2} x {:.2})",
+                    bbox.origin.x.get(),
+                    bbox.origin.y.get(),
+                    bbox.size.width.get(),
+                    bbox.size.height.get(),
+                );
+            }
+            println!(
+                "  pixel width x height: {:?} x {:?}",
+                img.xobject().width()?.get(),
+                img.xobject().height()?.get()
+            );
+            println!("  kind: {:?}", img.xobject().kind()?);
+            println!("  bpc: {:?}", img.xobject().bits_per_component()?);
+            println!("  stream filter: {:?}", img.xobject().filter()?);
+            println!("  has soft mask: {}", img.xobject().soft_mask().is_some());
         }
     }
     Ok(())
